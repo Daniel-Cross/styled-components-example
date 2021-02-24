a2a1c9904074cb05e624763264073bd6fe774ed7
Daniel-Cross <crossy1686@gmail.com>
2021-02-24 09:53:26 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = ({ name }) => {
+  return (
+    <header>
+      <h1>React Intro</h1>
+      <div>by {name}</div>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Header;
